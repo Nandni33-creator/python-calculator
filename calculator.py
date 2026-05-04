d1f6d641461f822ce4cc21a64c37283ae0fdee7c
Nandni33-creator <unand338@gmail.com>
2026-05-04 22:27:22 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+while True:
+    print("\nSimple Calculator")
+    
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == "+":
+        print("Result:", num1 + num2)
+
+    elif operator == "-":
+        print("Result:", num1 - num2)
+
+    elif operator == "*":
+        print("Result:", num1 * num2)
+
+    elif operator == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero")
+
+    else:
+        print("Invalid operator")
+
+    choice = input("Do you want to continue? (yes/no): ")
+    if choice.lower() != "yes":
+        break
